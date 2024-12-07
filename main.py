42d7f84acb52679e12da9f201718652cbe00af81
George Freedland <george.freedland@gmail.com>
2024-12-07 01:07:20 -0800

    simplify output

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from google.cloud import texttospeech
-from google.oauth2 import service_account
 from dotenv import load_dotenv
 import os
 import argparse
@@ -8,24 +7,15 @@ import argparse
 # Load environment variables from .env file
 load_dotenv()
 
-
 # Get the path to the service account JSON from the .env file
 credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
 
 # Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
 
 
-def text_to_speech(text, output_file):
-    output_file_name = output_file if output_file.endswith(".mp3") else output_file + ".mp3"
-
-    # If one were to productionize, you'd want to do this to hide your serivce account.
-    # credentials = service_account.Credentials.from_service_account_file("/path/to/service_account.json")
-    # client = texttospeech.TextToSpeechClient(credentials=credentials)
-
+def text_to_speech(text):
     client = texttospeech.TextToSpeechClient()
-
 
     input_text = texttospeech.SynthesisInput(text=text)
 
@@ -40,6 +30,7 @@ def text_to_speech(text, output_file):
         input=input_text, voice=voice, audio_config=audio_config
     )
 
+    output_file_name = "output.mp3"
     with open(output_file_name, "wb") as out:
         out.write(response.audio_content)
         print(f"Audio content written to {output_file_name}")
@@ -49,12 +40,11 @@ def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description='Convert text to speech')
     parser.add_argument('text', help='The text you want to convert')
-    parser.add_argument('output', help='The name of the output file')
     # Parse arguments
     args = parser.parse_args()
     
     # Call conversion function
-    text_to_speech(args.text, args.output)
+    text_to_speech(args.text)
 
 
 if __name__ == '__main__':
